91653b29b9990732f4158a0d15b8b5fb534d9caf
João José <joaojose@Joaos-MacBook-Pro.local>
2024-03-12 12:07:16 -0300

    features: LanguageModel and methods to_dict and list_dicts

---
+++ src/models/language_model.py
@@ -0,0 +1,22 @@
+from pymongo import ReturnDocument
+from src.database.db import db
+from models.abstract_model import AbstractModel
+
+
+class LanguageModel(AbstractModel):
+    _collection = db["languages"]  # Definindo o nome da coleção
+
+    def __init__(self, data):
+        super().__init__(data)
+    
+
+    def to_dict(self):
+        return {
+            "name": self.data.get("name"),
+            "acronym": self.data.get("acronym"),
+        }
+    
+    @classmethod
+    def list_dicts(cls):
+        languages = cls.find()
+        return [language.to_dict() for language in languages]
