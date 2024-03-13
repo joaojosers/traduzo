ae282d9e6b0daf355c8b52c266bb5765c0c233ac
João José <joaojose@Joaos-MacBook-Pro.local>
2024-03-12 23:33:58 -0300

    feature: controller reverse translation

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -48,25 +48,24 @@ def translate_text():
     )
 
 
-# @translate_controller.route("/", methods=["POST"])
-# def translate_text():
-#     # Receber parâmetros do corpo da solicitação
-#     text_to_translate = request.form.get("text_to_translate")
-#     translate_from = request.form.get("translate_from")
-#     translate_to = request.form.get("translate_to")
+@translate_controller.route("/reverse", methods=["POST"])
+def reverse_translation():
+    # Receber parâmetros do corpo da solicitação
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
 
-#     # Realizar a tradução do texto
-#     translated_text = GoogleTranslator(
-#         source=translate_from, target=translate_to
-#     ).translate(text_to_translate)
+    # Realizar a tradução reversa do texto
+    reversed_text = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
 
-#     # Retornar o texto traduzido
-#     # return jsonify({"translated": translated_text})
-#     return render_template(
-#         "index.html",
-#         text_to_translate=text_to_translate,
-#         translate_from=translate_from,
-#         translate_to=translate_to,
-#         translated=translated_text
-#         # languages = LanguageModel.list_dicts()
-#     )
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=reversed_text,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
