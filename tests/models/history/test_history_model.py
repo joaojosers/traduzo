f4d9ab1a4c8135a43d8bdb37887db20f1bf3d86c
João José <joaojose@Joaos-MacBook-Pro.local>
2024-03-13 14:39:35 -0300

    feature: test_history_model

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,13 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    json_tests = HistoryModel.list_as_json()
+    tests = json.loads(json_tests)
+
+    assert len(tests) == 2
+    assert tests[0]["text_to_translate"] == 'Hello, I like videogame'
+    assert tests[0]["translate_from"] == 'en'
+    assert tests[0]["translate_to"] == 'pt'
